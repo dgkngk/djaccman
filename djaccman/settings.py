7ac24cb8d1c78d643dfa4cf6e2dc60bdfdf6d6a4
dgkngk <dogukan_gok@outlook.com>
2024-04-24 14:51:04 +0300

    render deployment config

--- djaccman/settings.py
+++ djaccman/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(7fycbxi%p@^-o&fxe@kzcnh#56uhjdfeqx^$30dgy4!^fo%i4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
